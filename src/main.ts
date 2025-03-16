feb8f013756d3f87eb46cfd0b709e01f97d66691
hnamhocit <hnamhocit@gmail.com>
2025-03-16 14:37:42 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app.module'
-import { AuthGuard } from './common/guards/auth.guard'
-import { PrismaService } from './prisma/prisma.service'
+import { AppModule } from './app.module';
+import { AuthGuard } from './common/guards/auth.guard';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
